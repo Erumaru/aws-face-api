d0e490520f20b694b637f4aa1064c514aa3391d4
Abzal <toremuratuly.abzal@gmail.com>
2021-04-21 14:40:35 +0600

    Initial Commit

---
+++ AWSFaceAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWSFaceAPI
+//
+//  Created by Abzal Toremuratuly on 21.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
